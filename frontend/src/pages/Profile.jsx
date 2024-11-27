94ef604b292f3a9aacbb53db7ac75d3f1b159e48
Amaan ansari <amaan.ansari2080@gmail.com>
2024-11-27 16:52:22 +0530

    updated profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -160,7 +160,7 @@ const Profile = () => {
           </div>
           <button
             type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded"
+            className="bg-blue-500 text-white px-4 py-2 rounded "
           >
             Update Password
           </button>
